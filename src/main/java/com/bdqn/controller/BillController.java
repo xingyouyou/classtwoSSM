010d5439dae89bbc3db0d3a747e544a97ebf09a3
xqj <572175066@qq.com>
2019-09-16 14:55:33 +0800

    完成了订单控制器的编写。

---
+++ src/main/java/com/bdqn/controller/BillController.java
@@ -0,0 +1,17 @@
+package com.bdqn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName BillController
+ * @Description: TODO 订单控制器
+ * @Author: xqj
+ * @Date: 2019/9/16 14:51
+ * @Version v1.0
+ */
+@Controller
+@RequestMapping("/bill")
+public class BillController {
+
+}
